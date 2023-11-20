86a25fc5602974bf5cc3196a718c3a9cd20d2588
Michael M <mullinsmikey@users.noreply.github.com>
2023-11-20 10:37:19 +0100

    fix: image download

--- src/1-chains.ts
+++ src/1-chains.ts
@@ -1,53 +1,37 @@
 import { HfInference } from '@huggingface/inference';
 import 'dotenv/config';
-import { ConsoleCallbackHandler } from 'langchain/callbacks';
-import { HuggingFaceInference } from 'langchain/llms/hf';
 import { OpenAI } from 'langchain/llms/openai';
 import { PromptTemplate } from 'langchain/prompts';
-import { BytesOutputParser, StringOutputParser } from 'langchain/schema/output_parser';
+import { StringOutputParser } from 'langchain/schema/output_parser';
 import { RunnableSequence } from 'langchain/schema/runnable';
-import { NodeFileStore } from 'langchain/stores/file/node';
-import { WriteFileTool } from 'langchain/tools';
-import { createWriteStream, writeFileSync } from 'node:fs';
+import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
 const openAi = new OpenAI({ temperature: 1 });
-const hf = new HuggingFaceInference({ model: 'stabilityai/stable-diffusion-2' });
 const hfi = new HfInference(process.env.HUGGINGFACEHUB_API_KEY);
 
-const fileStore = new NodeFileStore(process.cwd());
-
-const promptJ = PromptTemplate.fromTemplate(
+const prompt = PromptTemplate.fromTemplate(
     `Translate this joke from {languageFrom} into {languageTo}: {joke}. Only respond with the result.`,
 );
 
-const chainJ = promptJ.pipe(openAi).pipe(new StringOutputParser());
+const chain = prompt.pipe(openAi).pipe(new StringOutputParser());
 
-const combinedChainJ = RunnableSequence.from([
-    chainJ,
-    // (imagePrompt: string) => hf.call(imagePrompt),
+const combinedChain = RunnableSequence.from([
+    chain,
     (imagePrompt: string) => hfi.textToImage({
         model: 'stabilityai/stable-diffusion-2',
-        inputs: imagePrompt,
+        inputs: imagePrompt.trim(),
         parameters: { negative_prompt: 'blurry' },
     }),
-    new BytesOutputParser(),
 ]);
 
-const blob = await combinedChainJ.invoke({
+const imageBlob = await combinedChain.invoke({
     joke: 'Šta kaže Brus Vilis kad ode u prodavnicu kompjuterske opreme? "Daj hard."',
     languageFrom: 'Serbian',
     languageTo: 'Chinese',
-}, { callbacks: [new ConsoleCallbackHandler()] });
+}/*, { callbacks: [new ConsoleCallbackHandler()] }*/);
+
+const imageBuf = await imageBlob.arrayBuffer();
 
 const filePath = join(process.cwd(), performance.now() + '.jpg');
-const file = createWriteStream(filePath);
-
-// for await (const chunk of blob) {
-//     if (chunk) chunk.pipe(file);
-// }
-// file.close();
-
-file.write(blob, () => file.close())
-
-// writeFileSync(filePath, Buffer.from(blob));
+await writeFile(filePath, Buffer.from(imageBuf));
